dde158a1d330b2cdb7f4521d5df1febfbc558943
MOHAMADBASHAR <77447431+MOHAMADBASHAR@users.noreply.github.com>
2022-02-24 10:29:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ btnOpen.addEventListener("click", function(){
     let email,passwor;
     email = document.getElementById(`userEmail`).value;
     passwor = document.getElementById(`userPassword`).value;
-    document.getElementById(`icon`).classList.remove(`basarili`,`basarisiz`)
+    document.getElementById(`icon`).classList.remove(`basarili`,`basarisiz`,`fa-circle-xmark`,`fa-circle-check`)
     let alertContent = document.getElementById(`alertText`).innerHTML = ``
     document.getElementById("alert").style.display = "flex";;
     document.getElementById("container").style.opacity = "0.1";;
     if(email == "muhammed@gmail.com" && passwor == "123"){
-        document.getElementById(`icon`).classList.add(`basarili`)
+        document.getElementById(`icon`).classList.add(`basarili`,`fa-circle-check`)
         let alertContent = document.getElementById(`alertText`).innerHTML = `GIRIS BASARILI`
     }
     else{
-        document.getElementById(`icon`).classList.add(`basarisiz`)
+        document.getElementById(`icon`).classList.add(`basarisiz`,`fa-circle-xmark`)
         let alertContent = document.getElementById(`alertText`).innerHTML = `GIRIS BASARISIZ`
     }
   });
