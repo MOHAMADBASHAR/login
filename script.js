8fc218bdd167ab1f2ab09a0ae38428827788ebbe
MOHAMADBASHAR <77447431+MOHAMADBASHAR@users.noreply.github.com>
2022-02-24 10:07:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const btnOpen = document.querySelector(`#button`);
+btnOpen.addEventListener("click", function(){
+    let email,passwor;
+    email = document.getElementById(`userEmail`).value;
+    passwor = document.getElementById(`userPassword`).value;
+    document.getElementById(`icon`).classList.remove(`basarili`,`basarisiz`)
+    let alertContent = document.getElementById(`alertText`).innerHTML = ``
+    document.getElementById("alert").style.display = "flex";;
+    document.getElementById("container").style.opacity = "0.1";;
+    if(email == "muhammed@gmail.com" && passwor == "123"){
+        document.getElementById(`icon`).classList.add(`basarili`)
+        let alertContent = document.getElementById(`alertText`).innerHTML = `GIRIS BASARILI`
+    }
+    else{
+        document.getElementById(`icon`).classList.add(`basarisiz`)
+        let alertContent = document.getElementById(`alertText`).innerHTML = `GIRIS BASARISIZ`
+    }
+  });
+  function closeAlert(){
+    document.getElementById("container").style.opacity = "1";;
+    document.getElementById("alert").style.display = "none";;
+  }
